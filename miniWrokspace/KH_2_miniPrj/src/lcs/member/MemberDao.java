22d0b8e5e7d868bd78457e687cb7bd6cd239aec2
DESKTOP-93QTN2S\xitarg15 <ckstjs97@gmail.com>
2022-08-02 22:06:57 +0900

    임찬선 0802 작업
    오늘 작업한 내용입니다.

---
+++ miniWrokspace/KH_2_miniPrj/src/lcs/member/MemberDao.java
@@ -0,0 +1,76 @@
+package lcs.member;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import lcs.member.MemberVo;
+import common.JDBCTemplate;
+
+public class MemberDao {
+	public MemberVo login(String inputId, String inputPwd) throws Exception {
+		// DB 가서 , id pwd 일치하는 행 조회
+		
+		// CONNECTION 준비
+		Connection conn = JDBCTemplate.getConnection();
+		
+		// SQL 작성
+		String sql = "SELECT NO , ID , NICK FROM MEMBER WHERE ID = ? AND PWD = ? ";
+		
+		// SQL 객체에 담기
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, inputId);	//대소문자 어찌하실지 고민해보세요~!
+		pstmt.setString(2, inputPwd);
+		
+		// SQL 실행
+		ResultSet rs = pstmt.executeQuery();
+		
+		MemberVo vo = null;
+		
+		if(rs.next()) {
+			String no = rs.getString("NO");
+			String id = rs.getString("ID");
+			String nick = rs.getString("NICK");
+			
+			vo = new MemberVo();
+			vo.setNo(no);
+			vo.setId(id);
+			vo.setNick(nick);
+		}
+		
+		return vo;
+		
+	}//login
+
+	public int join(MemberVo vo, Connection conn) throws Exception {
+		
+		//DB insert
+		
+		int result = 0;
+		PreparedStatement pstmt = null;
+		
+		try {
+			//SQL 준비
+			String sql = "INSERT INTO MEMBER(NO, ID, PWD, NICK) VALUES(SEQ_MEMBER_NO.NEXTVAL, ?, ?, ?)";
+			
+			//SQL 담을 객체 만들기
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, vo.getId());
+			pstmt.setString(2, vo.getPwd());
+			pstmt.setString(3, vo.getNick());
+			
+			//SQL 실행 및 결과 저장
+			result = pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			JDBCTemplate.close(pstmt);
+		}
+		
+		return result;
+		
+		
+	}//join
+}
--- miniWrokspace/KH_2_miniPrj/src/lcs/menu/Menu.java
+++ miniWrokspace/KH_2_miniPrj/src/lcs/menu/Menu.java
@@ -5,14 +5,16 @@ import util.InputUtil;
 public class Menu {
 	
 	
-	public int showLcs() {
+	public int showMenu() {
 		
 		
 		System.out.println("임찬선 - 동물훈련팁 부분입니다.");
 		
-		System.out.println("1. 동물훈련 팁");
-		System.out.println("2. 동물훈련소 보기");
-		System.out.println("3. 맨 처음 메뉴로 돌아가기");
+		System.out.println("1. 로그인");
+		System.out.println("2. 회원가입");
+		System.out.println("3. 동물훈련 팁");
+		System.out.println("4. 동물훈련소 보기");
+		System.out.println("5. 맨 처음 메뉴로 돌아가기");
 		
 		return InputUtil.getInt();
 		
