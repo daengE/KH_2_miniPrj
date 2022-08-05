012ed488ba1c15b280d83e6298639cb95f8136bf
DESKTOP-TBR36TN\User <ckstjs97@gmail.com>
2022-08-05 23:33:44 +0900

    임찬선 11시30분 커밋
    메인이랑 메뉴 받고싶어요

--- miniWrokspace/KH_2_miniPrj/src/lcs/application/ApplicationController.java
+++ miniWrokspace/KH_2_miniPrj/src/lcs/application/ApplicationController.java
@@ -1,6 +1,7 @@
 package lcs.application;
 
 import mini.main.Main;
+import mini.member.MemberController;
 import lcs.atc.ATCVo;
 import mini.util.InputUtil;
 
@@ -10,6 +11,11 @@ public class ApplicationController {
 	public void write(ATCVo atcvo) {
 		
 	
+		if (Main.loginMember == null) {
+			System.out.println();
+			System.out.println("먼저 로그인을 해주세요.");
+			new MemberController().login();
+		}
 		
 		//회원번호로 닉네임 받고 닉네임님 작성해주세요.
 		
@@ -20,16 +26,23 @@ public class ApplicationController {
 		System.out.println("----- 훈련소 입소 신청서 ----");
 		System.out.println("신청하실 훈련소 이름이 맞는지 확인 부탁드립니다.");
 		System.out.println("동물훈련소 이름 : " + atcvo.getName());
-		System.out.println("\n 신청하실 동물은 어떤 종류인가요?");
+		System.out.println();
+		
+		System.out.println("신청하실 동물은 어떤 종류인가요?");
 		System.out.print("동물의 종류 : ");
 		String animalType = InputUtil.sc.nextLine();
+		System.out.println();
+		
 		System.out.println("\n 연락가능한 핸드폰 번호를 작성해주세요(훈련소와 연결 시 필요합니다.)");
 		System.out.println("하이픈 제외하고 숫자만 작성해주세요.");
 		System.out.print("핸드폰번호 : ");
 		String phone = InputUtil.sc.nextLine();
+		System.out.println();
+		
 		System.out.println("\n 입소가능한 날짜를 알려주세요(형식 예 : 220802");
 		System.out.print("입소가능일자 : ");
 		String psbDate = InputUtil.sc.nextLine();
+		System.out.println();
 		
 		ApplicationVo aplyVo = new ApplicationVo();
 		aplyVo.setUserNo(mini.main.Main.loginMember.getNo());
